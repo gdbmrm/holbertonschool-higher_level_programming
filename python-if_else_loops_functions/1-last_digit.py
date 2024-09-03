db57f6e1a8da7cc568df85c21f8fd2afe6c94e9d
gdbmrm <>
2024-09-03 17:23:22 +0000

    ex 0 et 1 if statement

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_digit = number % 10
+str = f"Last digit of {number} is {last_digit} "
+
+if last_digit > 5:
+    print(str + "and is greater than 5")
+elif 0 < last_digit < 6:
+    print(str + "and is less than 6 and not 0")
+else:
+    print(str + "and is 0")
