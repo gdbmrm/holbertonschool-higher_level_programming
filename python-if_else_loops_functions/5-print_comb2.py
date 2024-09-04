c2878b5812d8b85c7f62764b5cd3bc6ff3188586
gdbmrm <>
2024-09-04 11:22:07 +0000

    modif ex 5

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -3,7 +3,5 @@
 for num in range(0, 100):
     if num == 99:
         print(f"{num}")
-    elif 0 <= num <= 9:
-        print(f"0{num}, ", end="")
     else:
-        print(f"{num}, ", end="")
+        print("{:02d}, ".format(num), end="")
