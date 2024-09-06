36786f027733b3398fdb96f6b0f0bc6c1b3639af
gdbmrm <>
2024-09-06 08:55:50 +0000

    ex 8

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
 def uppercase(str):
-    result = ""
     for charactere in str:
         if 97 <= ord(charactere) <= 122:
-            result += chr(ord(charactere) - 32)
-        else:
-            result = result + charactere
-    print(result)
+            charactere = chr(ord(charactere) - 32)
+        print("{}".format(charactere), end="")
+    print()
+ 
