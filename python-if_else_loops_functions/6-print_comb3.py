1ba24b476bab2c50b91c01dff7809d21992dbf40
gdbmrm <>
2024-09-06 08:24:23 +0000

    ex  6 correction

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -3,6 +3,7 @@
 for i in range(0, 9):
     for j in range(0, 10):
         if i != j and j > i:
-            print("{}{}, ".format(i, j), end="")
             if i == 8 and j == 9:
                 print("{}{}".format(i, j))
+            else:
+                print("{}{}, ".format(i, j), end="")
