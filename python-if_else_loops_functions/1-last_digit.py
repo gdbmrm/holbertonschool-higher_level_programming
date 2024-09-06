7da2c0be80998a40a0537e1347d6f87507bf0f06
gdbmrm <>
2024-09-06 07:48:33 +0000

    ex 1

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -9,7 +9,7 @@ if last_digit > 5:
     print(str + "and is greater than 5")
 elif last_digit == 0:
     print(str + "and is 0")
-elif last_digit == 0:
+elif  0 < last_digit < 6:
     print(str + "and is less than 6 and not 0")
 elif number < 0:
     last_digit = -last_digit
