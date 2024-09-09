a056ce8010e41857831fd5ffc6255894ac301f00
gdbmrm <>
2024-09-09 13:02:13 +0000

    ex 5 & 6

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+
+    i = 0
+    j = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if j != len(matrix) - 1:
+                print("{:d} ".format(matrix[i][j]), end="")
+            else:
+                print("{:d}".format(matrix[i][j]))
