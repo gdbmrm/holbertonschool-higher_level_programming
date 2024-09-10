a630362087a94de6e975e04a3b3ea4b44d6388ce
gdbmrm <>
2024-09-10 11:04:19 +0000

    ex 12

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,7 +4,7 @@ def delete_at(my_list=[], idx=0):
 
     new_list = []
 
-    if idx < 0 or idx >= (len(my_list) - 1):
+    if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
 
     for i in range(len(my_list)):
