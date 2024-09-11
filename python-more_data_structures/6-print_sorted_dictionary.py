c85df37a0743a046d9e972d1ad23402c5840aa3d
gdbmrm <>
2024-09-11 08:28:43 +0000

    exo 6

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -4,6 +4,5 @@ def print_sorted_dictionary(a_dictionary):
 
     item = sorted(a_dictionary.items())
 
-    for i in item:
-        print(i)
-    return(item)
+    for key, values in item:
+        print("{} : {}".format(key, values))
