77f590472ff2a5b42d1e909112200c41fbdb218d
gdbmrm <>
2024-09-11 14:05:30 +0000

    ex 1 2 3

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+
+    real_number = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            real_number += 1
+        except (IndexError, TypeError):
+            continue
+
+    print()
+    return real_number
