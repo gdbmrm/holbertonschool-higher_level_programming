028f786231f30fade79e5acd94244829adc0252b
gdbmrm <>
2024-09-11 10:08:14 +0000

    ex 11

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    mapped_numbers = list(map(lambda x: x * number, my_list))
-    return(mapped_numbers)
+    return(list(map(lambda x: x * number, my_list)))
