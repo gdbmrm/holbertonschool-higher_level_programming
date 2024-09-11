bd5aa9a4b9fb0e2da71ff8d1a83f30af284c3021
gdbmrm <>
2024-09-11 08:44:43 +0000

    ex 4

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
 
 def only_diff_elements(set_1, set_2):
+
+    diff_elements = set_1.union(set_2)
+
+    for element_1 in range(len(diff_elements) - 1):
+        for element_2 in range(len(diff_elements)):
+            if element_1 == element_2:
+                diff_elements.remove(element_2)
+
+
+    return(diff_elements)
     
