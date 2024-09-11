6d6304983b9fb178e0ccabf9174b6ef6e3688dbf
gdbmrm <>
2024-09-11 09:54:54 +0000

    ex 9 et 10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+
+    max = 0
+    max_key = ""
+
+    if bool(a_dictionary) is False:
+        return None
+
+    for key, item in a_dictionary.items():
+        if item > max:
+            max = item
+            max_key = key
+
+    return(max_key)
