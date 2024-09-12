d844417f3d6f74553f46807277831e07bba39125
gdbmrm <>
2024-09-12 13:26:48 +0000

    ex 0

--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 add_integer = __import__('0-add_integer').add_integer
 
-print(add_integer(3, 6))
-print(add_integer(100, -2))
-print(add_integer(2))
+print(add_integer(4, 4))
+print(add_integer(3.2, 6))
+print(add_integer("l", 3))
 print(add_integer(100.3, -2))
+f = float('-inf')
+print(add_integer(f, 6))
+print(add_integer(3, "b"))
+print(add_integer(2))
+
+
+
 try:
     print(add_integer(4, "School"))
 except Exception as e:
