0074b57bd80f390813c8b393dc718d3075cdd077
gdbmrm <>
2024-09-12 10:16:14 +0000

    ex 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -18,5 +18,8 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             resultat.append(0)
             print("out of range")
+            return (resultat)
+        finally:
+            pass
 
     return resultat
