defaf88691b91da46ba608afff01f595779e8ba9
gdbmrm <>
2024-09-12 14:36:47 +0000

    ex 2

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -11,6 +11,10 @@ def matrix_divided(matrix, div):
                 new_row.append(round(matrix[i][j] / div, 2))
             except ZeroDivisionError:
                 print("division by zero")
+                return new_matrix
+            except TypeError:
+                print("div must be a number")
+                return new_matrix
 
         new_matrix.append(new_row)
     return new_matrix
