b7e59bcd2ef3220ec989e7a2114ecc766b2a7852
gdbmrm <>
2024-09-12 09:21:44 +0000

    ex 9

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 def max_integer(my_list=[]):
 
-    maxi = my_list[0]
-
     if len(my_list) == 0:
         return None
+
+    maxi = my_list[0]
 
     for i in range(len(my_list)):
         if my_list[i] > maxi:
