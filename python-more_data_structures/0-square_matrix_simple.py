7d0b401061dcb7afba5ecec1899d9e0996588b0d
gdbmrm <>
2024-09-12 14:12:53 +0000

    ex 0

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,10 +2,12 @@
 
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-    i , j = 0, 0
-    
+    i, j = 0, 0
+
     for i in range(len(matrix)):
+        new_row = []
         for j in range(len(matrix[i])):
-            new_matrix.append(matrix[i][j] ** 2)
+            new_row.append(matrix[i][j] ** 2)
+        new_matrix.append(new_row)
 
     return new_matrix
---
+++ python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def matrix_divided(matrix, div):
+    i = 0
+    j = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            
