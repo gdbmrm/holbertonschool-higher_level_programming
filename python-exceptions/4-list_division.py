3a01b735aa24e2539558b2aa491f288dcc4a32ae
gdbmrm <>
2024-09-12 10:01:04 +0000

    ex 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -8,10 +8,15 @@ def list_division(my_list_1, my_list_2, list_length):
     for i in range(list_length + 1):
         try:
             if my_list_1[i] % my_list_2[i] == 0:
-                resultat.append(my_list_1[i] % my_list_2[i])
-            else:
-                resultat.append(0)
-        except Exception as e:
-            print(e)
-    
+                resultat.append(my_list_1[i] / my_list_2[i])
+        except ZeroDivisionError:
+            resultat.append(0)
+            print("division by 0")
+        except TypeError:
+            resultat.append(0)
+            print("wrong type")
+        except IndexError:
+            resultat.append(0)
+            print("out of range")
+
     return resultat
