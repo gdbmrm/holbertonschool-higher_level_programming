821fd4d7f82d13ffb8dd8d48c9a2fb3fc600745e
gdbmrm <>
2024-09-12 14:30:00 +0000

    ex 2 matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 
 def matrix_divided(matrix, div):
-    i = 0
-    j = 0
+    new_matrix = []
+    i, j = 0, 0
 
     for i in range(len(matrix)):
+        new_row = []
         for j in range(len(matrix[i])):
-            
+            try:
+                new_row.append(round(matrix[i][j] / div, 2))
+            except ZeroDivisionError:
+                print("division by zero")
+
+        new_matrix.append(new_row)
+    return new_matrix
