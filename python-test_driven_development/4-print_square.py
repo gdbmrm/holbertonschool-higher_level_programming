a505691b1e1745f35b1285cc409031fe36a48e39
gdbmrm <>
2024-09-13 13:47:26 +0000

    ex 4 pycode

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -7,7 +7,7 @@ def print_square(size):
 
     if size < 0:
         raise TypeError("size must be >= 0")
-    
+
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
 
@@ -15,4 +15,3 @@ def print_square(size):
         for j in range(size):
             print("#", end="")
         print()
-        
