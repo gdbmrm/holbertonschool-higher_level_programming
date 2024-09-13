92e23a1be1e62b11734e2d3151bf55810b8c2eee
gdbmrm <>
2024-09-13 13:57:02 +0000

    ex 5

---
+++ python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def text_indentation(text):
+
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    for i in text:
+        if i == "." or i == "?" or i == ":":
+            print(i, end="")
+            print()
+            print()
+        else:
+            print(i, end="")
+        
+    print()
