0a38f2c989523e6656a68ac20a89b5d0a77a96b6
gdbmrm <>
2024-09-16 08:51:06 +0000

    exo 0 et 1

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+Square module.
+This module contains a class that defines a square.
+"""
 class Square:
     """
     Empty class that define a square
---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    Class that set the size of a square
+    """
+    def __init__(self, size):
+        """ set the size of the square with a private attribute
+        """
+        self.__size = size
