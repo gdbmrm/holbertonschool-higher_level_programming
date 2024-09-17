cba164eaaf85cb6181b8efb6a3508595440c166b
gdbmrm <>
2024-09-17 10:02:54 +0000

    ex 0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Rectangle module.
+This module contains a class that defines a Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    empty class that create a rectangle
+    """
+    pass
