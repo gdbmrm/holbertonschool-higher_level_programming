a97c1616e4d7d94c234f38e68b5576d90941f044
gdbmrm <>
2024-09-17 14:36:01 +0000

    ex 4

---
+++ python-more_classes/4-rectangle.py
@@ -0,0 +1,90 @@
+#!/usr/bin/python3
+"""
+Rectangle module.
+This module contains a class that defines a Rectangle.
+"""
+
+
+class Rectangle:
+    """
+    class that create a rectangle
+    """
+    def __init__(self, width=0, height=0):
+        """
+        set the size of the rectangle
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """
+        return the width value
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        set the width value
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        return the height value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    def area(self):
+        """
+        return the area of a rectangle
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        calculate the perimeter of a rectangle
+        """
+        perimeter = 0
+        if self.__height == 0 or self.__width == 0:
+            perimeter = 0
+        else:
+            perimeter = self.__width * 2 + self.__height * 2
+        return perimeter
+
+    def __str__(self):
+        """
+        function that print a rectangle
+        """
+        rectangle = ""
+        if self.__width == 0 or self.__height == 0:
+            return rectangle
+        for i in range(self.__height):
+            for j in range(self.__width):
+                rectangle += "#"
+            if i < self.__height - 1:
+                rectangle += "\n"
+        return rectangle
+
+    def __repr__(self):
+        return self.__height,
+         self.__width
