70cd5b97b7d6c3ae9a2b5736e8d036ee42e61189
gdbmrm <>
2024-09-17 10:17:23 +0000

    ex 1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -13,13 +13,40 @@ class Rectangle:
         self.__width = width
         self.__height = height
 
+    @property
     def width(self):
+        """
+        return the width value
+        """
         return self.__width
 
+    @width.setter
     def width(self, value):
+        """
+        set the width value
+        """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        elif width < 0:
+        elif value < 0:
             raise ValueError("width must be >= 0")
         else:
-            
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        return the height value
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        set the height value
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
