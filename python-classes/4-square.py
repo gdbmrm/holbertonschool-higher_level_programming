8f51dea859c543acc77037dc95802de49c8540e4
gdbmrm <>
2024-09-20 07:22:54 +0000

    ex 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -14,7 +14,7 @@ class Square:
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        elif size <= 0:
+        if size < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = size
@@ -40,7 +40,7 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value <= 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
