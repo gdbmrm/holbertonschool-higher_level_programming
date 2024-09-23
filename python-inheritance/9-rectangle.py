b1d45750834090231be0c8575b6746f3aa11aeee
gdbmrm <>
2024-09-23 17:19:21 +0000

    ex9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+"""
+Geometry module
+"""
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        """
+        initialize width and height
+        """
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        """
+        print [Rectangle] <width>/<height>
+        """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+    def area(self):
+        """
+        return the area of the rectangle
+        """
+        return self.__width * self.__height
