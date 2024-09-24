237baa6f01794671711377490ad856ccd3715daa
gdbmrm <>
2024-09-24 09:11:46 +0000

    task 0

---
+++ python-abc/task_00_abc.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from abc import ABC, abstractmethod
+"""
+module animal
+"""
+
+class Animal(ABC):
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
