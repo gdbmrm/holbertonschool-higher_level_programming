6552426829fd8c47ab0dd9490e64d1fbee7dd461
gdbmrm <>
2024-09-24 13:11:13 +0000

    ex 5

---
+++ python-abc/task_05_dragon.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+class SwimMixin:
+    def swim(self):
+        print("The creature swims!")
+
+
+class FlyMixin:
+    def fly(self):
+        print("The creature flies!")
+
+
+class Dragon(SwimMixin, FlyMixin):
+    def roar(self):
+        print("The dragon roars!")
+
+
+draco = Dragon()
+draco.fly()
+draco.swim()
+draco.roar()
