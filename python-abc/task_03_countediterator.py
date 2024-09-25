cd3c0a77f8668b405662face4180d3e5436eaff6
gdbmrm <>
2024-09-25 11:53:11 +0000

    ex 3

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -4,10 +4,13 @@ class CountedIterator():
     def __init__(self, some_iterable):
         self.iterator = iter(some_iterable)
         self.count = 0
-    
+
     def __next__(self):
-        self.count += 1
-        if not next(self.iterator):
+        try:
+            self.count += 1
+            prochain = next(self.iterator)
+            return prochain
+        except StopIteration:
             raise StopIteration
 
     def get_count(self):
