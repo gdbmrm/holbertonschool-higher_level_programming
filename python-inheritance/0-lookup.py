5e79b7eeaa548675b2bb033139882a05cabcb64e
gdbmrm <>
2024-09-26 14:17:44 +0000

    ex0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-
-# fonction qui retourne la liste des attributs et méthodes d'une classe
+"""
+fonction qui retourne la liste des attributs et méthodes d'une classe
+"""
 def lookup(obj):
     """
     retourne les attributs et méthodes de la class obj
