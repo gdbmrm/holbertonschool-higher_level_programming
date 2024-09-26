b1804670e836fd6808792acbd8958e09a08a0487
gdbmrm <>
2024-09-26 16:18:13 +0000

    correction ex 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle(BaseGeometry):
         """
         initialize width and height
         """
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
         self.__width = width
         self.__height = height
