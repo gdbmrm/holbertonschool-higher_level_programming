157080dcae4ea36751957f1a833b96d944c053c8
gdbmrm <>
2024-09-27 10:46:31 +0000

    T7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -25,4 +25,4 @@ class BaseGeometry:
             raise TypeError("{} must be an integer".format(name))
         elif value <= 0:
             raise ValueError("{} must be greater than 0".format(name))
-        self.value = value
+        return value
