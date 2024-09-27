b3121e7f821184b6d2a08e73d103638f0761fb32
gdbmrm <>
2024-09-27 08:18:35 +0000

    t2

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -14,20 +14,20 @@ class VerboseList(list):
         append method
         """
         super().append(item)
-        print("Added {} to the list.".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, x):
         """
         extend method
         """
         super().extend(x)
-        print("Extended the list with {} items.".format(len(x)))
+        print("Extended the list with [{}] items.".format(len(x)))
 
     def remove(self, item):
         """
         remove method
         """
-        print("Removed {} from the list.".format(item))
+        print("Removed [{}] from the list.".format(item))
         super().remove(item)
 
     def pop(self, x=None):
@@ -36,5 +36,5 @@ class VerboseList(list):
         """
         if x is None:
             x = -1
-        print("Popped {} from the list.".format(self[x]))
+        print("Popped [{}] from the list.".format(self[x]))
         super().pop(x)
