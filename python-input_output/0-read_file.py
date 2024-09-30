6fcbb2e00351cc08248bcbec8e755000378eb8b1
gdbmrm <>
2024-09-30 13:45:38 +0000

    ex 0

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+fonction qui affiche le contenu d'un fichier
+"""
+
+
+def read_file(filename=""):
+    with open(filename, "r") as file_to_open:
+        for line in file_to_open:
+            print(line, end="")
