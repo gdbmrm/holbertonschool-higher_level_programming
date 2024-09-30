968a4a26134d32a0cdebb9ca0794a5e20b4b89ff
gdbmrm <>
2024-09-30 13:55:51 +0000

    ex 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ fonction qui affiche le contenu d'un fichier
 
 
 def read_file(filename=""):
+    """
+    fonction lecture de fichier
+    """
     with open(filename, "r") as file_to_open:
         for line in file_to_open:
             print(line, end="")
