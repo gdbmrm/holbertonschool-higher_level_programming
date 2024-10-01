ac0aa7f38f13181463c5e08a6417f8df64dc64bd
gdbmrm <>
2024-10-01 20:48:46 +0000

    ex 8

---
+++ python-input_output/8-class_to_json.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+fonction qui renvoie la description du dictionnaire
+avec une structure de données simple
+(liste, dictionnaire, chaîne, nombre entier et booléen)
+pour la sérialisation JSON d’un objet
+"""
+import json
+
+
+def class_to_json(obj):
+    """
+    fonction class to json
+    """
+    return obj.__dict__
