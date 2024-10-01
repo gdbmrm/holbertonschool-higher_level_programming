c00e72bac86244675a0dd72315fa78acff79ad46
gdbmrm <>
2024-10-01 08:18:08 +0000

    ex 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,17 @@
 #!/usr/bin/python3
 import sys
+import os.path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """
 fonction qui transforme en liste les arguments puis
 les integre dans le fichier add_item.json dans un format json
 """
+if os.path.isfile("add_item.json"):
+    liste = load_from_json_file("add_item.json")
+else:
+    liste = []
 
-liste = []
 for index in range(1, len(sys.argv)):
     liste.append(sys.argv[index])
 save_to_json_file(liste, "add_item.json")
