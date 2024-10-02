33ab008aecb1535316b55e7b2be5611936817095
gdbmrm <>
2024-10-02 08:14:40 +0000

    ex 7

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def pascal_triangle(n):
+    liste = []
+
+    if n <= 0:
+        return liste
+    
+    
--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-import sys
-import os.path
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 """
 fonction qui transforme en liste les arguments puis
 les integre dans le fichier add_item.json dans un format json
 """
+import sys
+import os.path
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 
 liste = []
 for index in range(1, len(sys.argv)):
