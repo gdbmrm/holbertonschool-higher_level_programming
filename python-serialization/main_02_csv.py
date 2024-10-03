294b670575ae4b09f04d530b69e105e5275145b8
gdbmrm <>
2024-10-03 12:54:51 +0000

    task 02

--- python-serialization/main_02_csv.py
+++ python-serialization/main_02_csv.py
@@ -1,9 +1,6 @@
 #!/usr/bin/env python3
 from task_02_csv import convert_csv_to_json
-import csv
 
-# Exemple d'utilisation
-if __name__ == "__main__":
-    csv_file = "data.csv"
-    convert_csv_to_json(csv_file)
-    convert_csv_to_json
+csv_file = "data.csv"
+convert_csv_to_json(csv_file)
+print(f"Data from {csv_file} has been converted to data.json")
--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,19 +1,26 @@
 #!/usr/bin/env python3
-
+"""
+The objective of this exercise is to gain experience
+in reading data from one format (CSV)
+and converting it into another format (JSON)
+using serialization techniques.
+"""
 import csv
 import json
 
 def convert_csv_to_json(csv_filename):
-
+    """
+    fonction convert csv to json
+    """
     try:
         liste = []
         with open(csv_filename, "r") as file_csv:
             file_read = csv.reader(file_csv)
             for row in file_read:
-                liste.append(csv.DictReader(row))
+                liste.append(row)
+        liste_de_dico = json.dumps(liste)
         with open("data.json", "w+") as file_data:
-            liste = json.dumps(liste)
-            file_data.write(liste)
+            file_data.write(liste_de_dico)
             return True
     except FileNotFoundError:
         return False
