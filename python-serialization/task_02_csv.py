6e601186397b6a40d8ff601d96dc2377300df643
gdbmrm <>
2024-10-03 12:58:14 +0000

    task 02

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -15,12 +15,12 @@ def convert_csv_to_json(csv_filename):
     try:
         liste = []
         with open(csv_filename, "r") as file_csv:
-            file_read = csv.reader(file_csv)
+            file_read = csv.DictReader(file_csv)
             for row in file_read:
                 liste.append(row)
-        liste_de_dico = json.dumps(liste)
-        with open("data.json", "w+") as file_data:
-            file_data.write(liste_de_dico)
+        liste_de_liste = json.dumps(liste)
+        with open("data.json", "w") as file_data:
+            file_data.write(liste_de_liste)
             return True
     except FileNotFoundError:
         return False
