0ab19e7cfc3f1e8cca57abde3ed811f44610bb8f
gdbmrm <>
2024-10-04 08:07:20 +0000

    t7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -8,9 +8,10 @@ import os.path
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
+filename = "add_item.json"
 
-if os.path.exists("add_item.json"):
-    liste = load_from_json_file("add_item.json")
+if os.path.exists(filename):
+    liste = load_from_json_file(filename)
 else:
     liste = []
 
