8a30ac2359112598120c4c6c1df9011b6116eb77
gdbmrm <>
2024-10-04 08:03:50 +0000

    task 07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,7 +9,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-liste = []
+if os.path.exists(filename):
+    liste = load_from_json_file(filename)
+else:
+    liste = []
+
 for index in range(1, len(sys.argv)):
     liste.append(sys.argv[index])
 save_to_json_file(liste, "add_item.json")
