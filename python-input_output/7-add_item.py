6eeba1de177647460bc98ad668ee567bd7315e23
gdbmrm <>
2024-10-04 10:47:53 +0000

    task 7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,6 +5,8 @@ les integre dans le fichier add_item.json dans un format json
 """
 import sys
 import os
+import json
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
