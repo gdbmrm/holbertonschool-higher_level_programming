6c190dcec5b2d810434c3e45bd378b3a26c71295
gdbmrm <>
2024-10-06 19:46:49 +0000

    task 0

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -2,20 +2,20 @@
 """
 module basic serialization
 """
-import pickle
+import json
 
 
 def serialize_and_save_to_file(data, filename):
     """
     serialize and save data to the specified file
     """
-    with open(filename, "wb") as file_to_open:
-        return pickle.dump(data, file_to_open)
+    with open(filename, "w") as file_to_open:
+        json.dump(data, file_to_open)
 
 
 def load_and_deserialize(filename):
     """
     load and deserialize data from the specified file
     """
-    with open(filename, "rb") as file_to_open:
-        return pickle.load(file_to_open)
+    with open(filename, "r") as file_to_open:
+        return json.load(file_to_open)
