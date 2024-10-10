18360a0b5820dccf792e562267bc7cb99cde6200
gdbmrm <>
2024-10-10 10:05:41 +0000

    t2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
+"""
+ Consuming and processing data from an API using Python
+"""
 import requests
+import json
+import csv
+
 
 def fetch_and_print_posts():
-    response = requests.get()
-    print(response.status_code)
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
+    print("Status code: {}".format(response.status_code))
 
     if response.status_code == 200:
         parsed_data = response.json()
@@ -13,13 +19,13 @@ def fetch_and_print_posts():
 
 def fetch_and_save_posts():
 
-    response = requests.get()
+    response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     if response.status_code == 200:
         parsed_data = response.json()
         
-    dict_id = {post["id"]: post for post, values in parsed_data}
-    dict_title = {post["title"]: post for post, values in parsed_data}
-    dict_body = {post["body"]: post for post, values in parsed_data}
+    dict_id = [{"id": parsed_data["id"]} for cle, valeur in parsed_data.items() if "id" in parsed_data]
+    dict_title = [{"title": parsed_data["title"]} for cle, valeur in parsed_data.items() if "title" in parsed_data]
+    dict_body = [{"body": parsed_data["body"]} for cle, valeur in parsed_data.items() if "body" in parsed_data]
 
-    with open("posts.csv", "w"):
-        csv.DictWriter("posts.csv")
+    with open("posts.csv", "w+") as file_to_write:
+        file_writer = csv.DictWriter(file_to_write, fieldnames=["id", "title", "body"])
