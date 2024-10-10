8cd3c42ebf41ae0a088cce2a1e743efb1866f1a4
gdbmrm <>
2024-10-10 11:17:15 +0000

    task 02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
- Consuming and processing data from an API using Python
+Consuming and processing data from an API using Python
 """
 import requests
 import json
@@ -8,6 +8,9 @@ import csv
 
 
 def fetch_and_print_posts():
+    """
+    function fetch and print posts
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     print("Status code: {}".format(response.status_code))
 
@@ -17,15 +20,27 @@ def fetch_and_print_posts():
     for titles in parsed_data:
         print(titles)
 
-def fetch_and_save_posts():
 
+def fetch_and_save_posts():
+    """
+    fetch and save posts
+    """
     response = requests.get("https://jsonplaceholder.typicode.com/todos/1")
     if response.status_code == 200:
         parsed_data = response.json()
-        
-    dict_id = [{"id": parsed_data["id"]} for cle, valeur in parsed_data.items() if "id" in parsed_data]
-    dict_title = [{"title": parsed_data["title"]} for cle, valeur in parsed_data.items() if "title" in parsed_data]
-    dict_body = [{"body": parsed_data["body"]} for cle, valeur in parsed_data.items() if "body" in parsed_data]
+
+    dict_id = [
+        {"id": parsed_data["id"]}
+        for cle, valeur in parsed_data.items() if "id" in parsed_data]
+
+    dict_title = [
+        {"title": parsed_data["title"]}
+        for cle, valeur in parsed_data.items() if "title" in parsed_data]
+
+    dict_body = [
+        {"body": parsed_data["body"]}
+        for cle, valeur in parsed_data.items() if "body" in parsed_data]
 
     with open("posts.csv", "w+") as file_to_write:
-        file_writer = csv.DictWriter(file_to_write, fieldnames=["id", "title", "body"])
+        file_writer = csv.DictWriter(
+            file_to_write, fieldnames=["id", "title", "body"])
