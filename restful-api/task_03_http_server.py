d1dcf3dd5195d86229b4e051702e0ca389e18885
gdbmrm <>
2024-10-10 13:12:40 +0000

    t3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -1,12 +1,18 @@
 #!/usr/bin/python3
-import http.server
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-class My_SubClass(http.server.BaseHTTPRequestHandler):
-    """
-    sub class
-    """
+class MyHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header("Content-type", "text/html")
+        self.send_header('Content-type', 'text/html')
         self.end_headers()
-        self.wfile.write(b"<html><body><h1>Hello, this is a simple API!</h1></body></html>")
+        self.wfile.write(b"<html><body><h1>Hello, world!</h1></body></html>")
+
+def run(server_class=HTTPServer, handler_class=MyHandler, port=8000):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    print(f'Serving HTTP on port {port}...')
+    httpd.serve_forever()
+
+if __name__ == "__main__":
+    run()
