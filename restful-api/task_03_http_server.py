05fb2a135378851de727db9ea74510c4639fa009
gdbmrm <>
2024-10-11 12:02:39 +0000

    t3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -14,10 +14,21 @@ class My_SubClass(BaseHTTPRequestHandler):
         method do_get that handle get request
         """
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
         self.end_headers()
         self.wfile.write(b'Hello, this is a simple API!')
+        if "\data" in self.path:
+            try:
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                my_dict = {"name": "John", "age": 30, "city": "New York"}
+                dataset = json.dump(my_dict)
+                self.wfile.write(dataset) 
+            except:
+                elf.send_response(404)
+                self.send_header('Content-type', 'application/json')
+                self.end_headers()
+                self.wfile.write(b'404 - Not Found')
 
 
-httpd = HTTPServer(('', 8080), My_SubClass)
+httpd = HTTPServer(('', 8000), My_SubClass)
 httpd.serve_forever()
