ae346159f25e19357b04a6792114e51b9842214d
gdbmrm <>
2024-10-14 13:50:13 +0000

    task 04

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -15,22 +15,27 @@ users = {"jane": {"name": "Jane", "age": 28, "city": "Los Angeles"}}
 def home():
     return "Welcome to the Flask API!"
 
+
 @app.route('/data')
 def return_username():
-    return jsonify(users.keys())
+    return jsonify(list(users.keys()))
+
 
 @app.route('/status')
 def check_status():
     return "OK"
 
+
 @app.route('/users/<username>')
 def show_user_profile(username):
     return users[username]
 
+
 @app.route('/add_user', methods=['GET', 'POST'])
 def add_user(new_user):
-    users[new_user.get("username")] = new_user.get["key"]
-    return "Your account has been registered", 201
+    if request.method == 'POST':
+        users[new_user.get("username")] = new_user.get["key"]
+        return "Your account has been registered", 201
 
 
 if __name__ == "__main__":
