7c35cd63a14cd471f80802d71f2baacd508bef5b
gdbmrm <>
2024-10-14 09:12:21 +0000

    task 03

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -21,20 +21,23 @@ class My_SubClass(BaseHTTPRequestHandler):
                 self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 ### conversion dictionnaire en json et en bytes pour affichage
-                my_dict = {"name": "John", "age": 30, "city": "New York"}
+                my_dict = {"name": "John", "age": 30, "city": "New York\n"}
                 dataset = json.dumps(my_dict).encode('utf-8')
                 self.wfile.write(dataset) 
             except:
                 ### si erreur status code 404
                 self.send_response(404)
-                self.send_header('Content-type', 'application/json')
                 self.end_headers()
                 self.wfile.write(b'Endpoint not found')
+        elif self.path == "/status":
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(b'OK\n')
         else:
             ### sinon afficher une string
             self.send_response(200)
             self.end_headers()
-            self.wfile.write(b'Hello, this is a simple API!')
+            self.wfile.write(b'Hello, this is a simple API!\n')
 
 
 httpd = HTTPServer(('', 8000), My_SubClass)
