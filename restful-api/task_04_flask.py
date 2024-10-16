40ebbd636934e766327b4d0a32e72a577343a86a
gdbmrm <>
2024-10-16 10:08:31 +0000

    t4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -60,38 +60,41 @@ def add_user():
     """
     my_dict = {}
     new_user = request.get_json()
-    if new_user is not None and new_user.get("username") not in users:
-        if not request.json.get("username"):
-            return jsonify({
-                "error": "Username is required"}), 400
-        username = new_user.get("username")
+    if new_user.get("username") not in users:
+        return jsonify({"error": "User already registered"}), 409
 
-        if not request.json.get("name") or not isinstance(name, str):
-            return jsonify({
-                "error": "Name is required in string format"}), 400
-        name = new_user.get("name")
-
-        if not request.json.get("age") or not isinstance(age, int):
-            return jsonify({
-                "error": "Age is required in int format"}), 400
-        age = new_user.get("age")
-
-        if not request.json.get("city") or not isinstance(city, str):
-            return jsonify({
-                "error": "City is required in string format"}), 400
-        city = new_user.get("city")
-
-        users[username] = {
-            "name": name,
-            "age": age,
-            "city": city
-        }
-        return jsonify({
-            "message": "User added",
-            "user": new_user
-            }), 201
-    else:
+    if new_user is not None:
         return jsonify({"error": "No data"}), 400
+
+    if not request.json.get("username"):
+        return jsonify({"error": "Username is required"}), 400
+    username = new_user.get("username")
+
+    if not request.json.get("name") or not isinstance(name, str):
+        return jsonify({
+            "error": "Name is required in string format"}), 400
+    name = new_user.get("name")
+
+    if not request.json.get("age") or not isinstance(age, int):
+        return jsonify({
+            "error": "Age is required in int format"}), 400
+    age = new_user.get("age")
+
+    if not request.json.get("city") or not isinstance(city, str):
+        return jsonify({
+            "error": "City is required in string format"}), 400
+    city = new_user.get("city")
+
+    users[username] = {
+        "name": name,
+        "age": age,
+        "city": city
+    }
+    return jsonify({
+        "message": "User added",
+        "user": new_user
+        }), 201
+
 
 
 if __name__ == "__main__":
