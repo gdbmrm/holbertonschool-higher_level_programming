28cb204102cf6314c5eb716839fdd28ec8ce488b
gdbmrm <>
2024-10-19 16:15:32 +0000

    task 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -12,8 +12,16 @@ jwt = JWTManager(app)
 
 
 users = {
-    "user1": {"username": "user1", "password": generate_password_hash("password"), "role": "user"},
-    "admin1": {"username": "admin1", "password": generate_password_hash("password"), "role": "admin"}
+    "user1": {
+        "username": "user1",
+        "password": generate_password_hash("password"),
+        "role": "user"
+    },
+    "admin1": {
+        "username": "admin1",
+        "password": generate_password_hash("password"),
+        "role": "admin"
+    }
 }
 
 secret = secrets.token_hex(32)
@@ -29,6 +37,7 @@ def check_user(username, password):
             check_password_hash(users.get(username)["password"], password):
         return username
     return None
+
 
 @app.route('/basic-protected')
 @auth.login_required
@@ -47,12 +56,21 @@ def login():
     username = request.json.get("username", None)
     password = request.json.get("password", None)
 
-    user = User.query.filter_by(username=username).one_or_none()
-    if not user or not user.check_password(password):
-        return jsonify({"error":"Wrong username or password"}), 401
+    user = users.get(username)
 
-    access_token = create_access_token(identity=user)
+    if not user:
+        return jsonify({"error": "Invalid user"}), 401
+
+    passwd = check_password_hash(user["password"], password)
+
+    if passwd is False:
+        return jsonify({"error": "Invalid user"}), 401
+
+    access_token = create_access_token(identity={
+        "username": username,
+        "role": user["role"]})
     return jsonify(access_token=access_token), 200
+
 
 @app.route('/jwt-protected')
 @jwt_required()
@@ -62,46 +80,68 @@ def jwt_protected_route():
     """
     return "JWT Auth: Access Granted", 200
 
+
 @app.route('/admin-only')
-@auth.login_required(role='admin')
+@jwt_required()
 def admins_only():
     """
+    check if the user is an admin
     """
+    username = request.json.get("username", None)
+    user = users.get(username)
+
+    if get_jwt_identity()["role"] not "admin":
+        return jsonify({"error": "Admin access required"}), 403
+    return "Admin Access: Granted"
+
 
 @jwt.unauthorized_loader
 def handle_unauthorized_error(err):
     """
     handle_unauthorized_error
     """
-  return jsonify({"error": "Missing or invalid token"}), 401
+    return jsonify({"error": "Missing or invalid token"}), 401
+
+
+@jwt.unauthorized_loader
+def handle_unauthorized_error(err):
+    """
+    unauthorized error
+    """
+    return jsonify({"error": "Missing or invalid token"}), 401
+
 
 @jwt.invalid_token_loader
 def handle_invalid_token_error(err):
     """
-    handle_invalid_token_error
+    invalid token loader
     """
-  return jsonify({"error": "Invalid token"}), 401
+    return jsonify({"error": "Invalid token"}), 401
+
 
 @jwt.expired_token_loader
 def handle_expired_token_error(err):
     """
-    handle_expired_token_error
+    handle expired token error
     """
     return jsonify({"error": "Token has expired"}), 401
+
 
 @jwt.revoked_token_loader
 def handle_revoked_token_error(err):
     """
-    handle_revoked_token_error
+    handle revoked token error
     """
     return jsonify({"error": "Token has been revoked"}), 401
+
 
 @jwt.needs_fresh_token_loader
 def handle_needs_fresh_token_error(err):
     """
-    handle_needs_fresh_token_error
+    handle needs fresh token error
     """
     return jsonify({"error": "Fresh token required"}), 401
+
 
 if __name__ == '__main__':
     app.run
