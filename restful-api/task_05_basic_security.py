ba917ced0e6d49b2d18d2407fbea6454b8c1d21e
gdbmrm <>
2024-10-20 21:52:22 +0000

    task 5

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -3,7 +3,8 @@
 API Security and Authentication Techniques
 """
 from flask import Flask, jsonify, request
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from flask_jwt_extended import JWTManager, create_access_token
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
@@ -26,6 +27,7 @@ users = {
         "role": "admin"
     }
 }
+
 
 @auth.verify_password
 def check_user(username, password):
