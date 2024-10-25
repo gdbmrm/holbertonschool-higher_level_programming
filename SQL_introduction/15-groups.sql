315415c12b6a28323e920f2dada2f97044b3b163
gdbmrm <>
2024-10-25 11:22:15 +0000

    TASK 15 ET 16

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,2 +1,3 @@
 -- script that lists the number of records with the same score in the table second_table of the database hbtn_0c_0 in your MySQL server
-SELECT score, COUNT(*) number FROM second_table GROUP BY score HAVING number >= 1;
+SELECT score, COUNT(*) number FROM second_table GROUP BY score HAVING number >= 1
+ORDER BY score DESC;
