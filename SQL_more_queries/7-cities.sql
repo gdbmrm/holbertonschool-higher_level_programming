bc44dbbc0a93e4eee9114277269b2423deb24d25
gdbmrm <>
2024-10-28 14:33:26 +0000

    task 7 8 et 9

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -5,7 +5,8 @@ USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
     id INT UNIQUE AUTO_INCREMENT NOT NULL,
-    state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
+    state_id INT NOT NULL,
     name VARCHAR(256) NOT NULL,
     PRIMARY KEY (id),
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,4 @@
+-- script that lists all cities contained in the database hbtn_0d_usa
+SELECT cities.id, cities.name
+FROM cities
+ORDER BY cities.id ASC;
