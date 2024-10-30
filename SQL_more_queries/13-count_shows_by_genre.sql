3e9d03c5e9b721a73517fd471c000e6c1cc57db3
gdbmrm <>
2024-10-30 14:29:50 +0000

    task 0 1 2 et 3

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,4 +1,5 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM hbtn_0d_tvshows.tv_genres, hbtn_0d_tvshows.tv_show_genres
-GROUP BY tv_genres.name;
+FROM tv_genres, tv_show_genres, tv_shows
+LEFT JOIN tv_show_genres AS tgenre ON tv_shows.id = tgenre.show_id
+GROUP BY tv_shows.id;
---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+script that lists all states with a name
+starting with N (upper N) from the database hbtn_0e_0_usa
+"""
+import MySQLdb, sys
+
+database = MySQLdb.connect(
+    host="localhost",
+    port=3306,
+    user="root",
+    passwd="root",
+    db="hbtn_0e_0_usa",
+    charset="utf8")
+
+
+cur = database.cursor()
+cur.execute("SELECT * FROM states WHERE name ='{}'".format(sys.argv[4]))
+query_rows = cur.fetchall()
+for row in query_rows:
+    print(row)
+cur.close()
+database.close()
