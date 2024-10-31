c19d2fb40e651ef878551a757092cbc484e262d8
gdbmrm <>
2024-10-31 09:22:38 +0000

    t13 t14

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,7 @@
 -- script that lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
-FROM tv_genres, tv_show_genres, tv_shows
-LEFT JOIN tv_show_genres AS tgenre ON tv_shows.id = tgenre.show_id
-GROUP BY tv_shows.id;
+FROM tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+GROUP BY tv_genres.name
+ORDER BY number_of_shows DESC;
