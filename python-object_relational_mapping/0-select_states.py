91a18bed6691bc2316095c6723625aac8f315faa
gdbmrm <>
2024-11-02 15:48:56 +0000

    pycode task 0 et 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -3,14 +3,16 @@
  script that lists all states from the database hbtn_0e_0_usa
 """
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     database = MySQLdb.connect(
         host="localhost",
-        port=3306, user="root",
-        passwd="root",
-        db="hbtn_0e_0_usa",
-        charset="utf8")
+        port=3306,
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
 
     cur = database.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,15 +4,16 @@ script that lists all states with a name
 starting with N (upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
+import sys
 
 if __name__ == "__main__":
     database = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user="root",
-        passwd="root",
-        db="hbtn_0e_0_usa",
-        charset="utf8")
+        user=sys.argv[1],
+        password=sys.argv[2],
+        database=sys.argv[3]
+        )
 
     cur = database.cursor()
     cur.execute(
