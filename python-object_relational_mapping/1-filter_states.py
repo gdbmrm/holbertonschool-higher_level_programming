4daf856bb159ae18e260c674e9402aea8e6ee375
gdbmrm <>
2024-11-02 15:44:29 +0000

    task 0 et 1

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -5,19 +5,21 @@ starting with N (upper N) from the database hbtn_0e_0_usa
 """
 import MySQLdb
 
-database = MySQLdb.connect(
-    host="localhost",
-    port=3306,
-    user="root",
-    passwd="root",
-    db="hbtn_0e_0_usa",
-    charset="utf8")
+if __name__ == "__main__":
+    database = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user="root",
+        passwd="root",
+        db="hbtn_0e_0_usa",
+        charset="utf8")
 
-
-cur = database.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-query_rows = cur.fetchall()
-for row in query_rows:
-    print(row)
-cur.close()
-database.close()
+    cur = database.cursor()
+    cur.execute(
+        "SELECT states.id, states.name "
+        "FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    database.close()
