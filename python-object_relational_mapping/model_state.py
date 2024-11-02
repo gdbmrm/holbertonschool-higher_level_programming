63c4044915a54d7e58785c30040e24982a5dff0e
gdbmrm <>
2024-11-02 17:48:40 +0000

    task 6 models

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
-
+"""
+First state model
+"""
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
+    """
+    class State
+    """
     __tablename__ = 'states'
-    id = Column(unique Integer, primary_key=True, not null, auto increment)
-    name =  Column(String(128), not null)
+    id = Column(
+        Integer,
+        primary_key=True, autoincrement=True,
+        nullable=False, unique=True)
+    name = Column(String(128), nullable=False)
