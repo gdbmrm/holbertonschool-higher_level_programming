95d7a2b2ae472854e26c38c5cc47c604cf80ce5c
gdbmrm <>
2024-11-02 14:13:26 +0000

    correction des tasks

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -4,4 +4,5 @@ FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
-GROUP BY tv_genres.name;
+GROUP BY tv_genres.name
+ORDER BY tv_genres.name ASC;
