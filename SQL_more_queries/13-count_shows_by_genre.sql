cb5cef0673a5782aebae9d804b10791effe55831
gdbmrm <>
2024-11-02 14:20:09 +0000

    task 1 et 13

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,6 +2,5 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.genre_id) AS number_of_shows
 FROM tv_genres
 LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
