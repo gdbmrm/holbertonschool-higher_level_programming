62144dcb5913a06b05141d1d0ef8f1d2fb756f19
gdbmrm <>
2024-11-03 22:06:04 +0000

    task 1 et 2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,8 @@ if __name__ == "__main__":
         )
 
     cur = database.cursor()
-    cur.execute("SELECT * FROM states WHERE name ='{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states WHERE name ='{}' \
+        ORDER BY states.id ASC;".format(sys.argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
