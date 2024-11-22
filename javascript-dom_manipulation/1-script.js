c5660d0b672263d41cdb6e9b4fd1d1448ec1397c
gdbmrm <>
2024-11-22 11:26:06 +0000

    task 1, 2, 3

---
+++ javascript-dom_manipulation/1-script.js
@@ -0,0 +1,4 @@
+function myScript() {
+  document.querySelector("header").style.color = "#FF0000";
+}
+document.getElementById("red_header").addEventListener("click", myScript);
---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,4 @@
+function myScript() {
+    document.querySelector("header").classList.add("red");
+}
+document.getElementById("red_header").addEventListener("click", myScript);
